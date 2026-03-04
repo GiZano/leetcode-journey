d5f9d96fd4be3b229b7935c6386d85a013aca6fc
GiZano <giozanotti07@gmail.com>
2026-03-04 12:23:09 +0100

    Add: solution for problem 643 Fix: problem 392 file format

---
+++ solutions/easy/0643-maximum-average-subarray-I.py
@@ -0,0 +1,31 @@
+"""
+Problem: 643. Maximum Average Subarray I
+Category: Arrays / Sliding Window
+
+Description: You are given an integer array nums consisting of n elements, and an integer k.
+             Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value.
+
+Algorithm:
+
+- Calculate the sum of the first window
+- Iterate over each value starting from the right of the first window
+- Calculate the new sum by adding the new value and removing the value in position [i - k]
+- Keep the max total
+- Return the max total devided by k 
+
+Complexity:
+- Time: O(n)    # Iterate over the list once
+- Space: O(1)   # Only use integer variables
+
+"""
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        max_total = sum(nums[:k])
+        total = max_total
+
+        for i in range(k, len(nums)):
+            total = total - nums[i - k] + nums[i]
+            max_total = max(max_total, total)
+
+        return max_total/k
